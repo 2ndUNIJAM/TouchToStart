cc683aaeb2fcc5d78e33267ae70827d4b53276b1
JY_xps <0327max@gmail.com>
2024-01-28 01:31:46 +0900

    add cart Script&add prefab Colliders

--- Assets/Scripts/Controller/Cart.cs
+++ Assets/Scripts/Controller/Cart.cs
@@ -1,8 +1,51 @@
-﻿using UnityEngine;
+﻿using JetBrains.Annotations;
+using UnityEngine;
 
 namespace TouchToStart
 {
     public class Cart : Controller
     {
+        protected override Vector2 CalculateOutput()
+        {
+            if (upCollider.OverlapPoint(mousePosition)) { return new Vector2(0, 1f); }
+            else if (downCollider.OverlapPoint(mousePosition)) { return new Vector2(0, -1f); }
+            else if (leftCollider.OverlapPoint(mousePosition))
+            {
+                SameDepthMouse.transform.eulerAngles += new Vector3(0, 0, angularSpeed * Time.deltaTime);
+                return Vector2.zero;
+            }
+            else if (rightCollider.OverlapPoint(mousePosition))
+            {
+                SameDepthMouse.transform.eulerAngles -= new Vector3(0, 0, angularSpeed * Time.deltaTime);
+                return Vector2.zero;
+            }
+            else { return Vector2.zero;  }
+
+            //return base.CalculateOutput();
+        }
+
+        public float angularSpeed;
+
+        protected override bool IsHovering()
+        {
+            mousePosition = SameDepthMouse.transform.position;
+            return !centerCollider.OverlapPoint(mousePosition);
+
+            // return base.IsHovering();
+        }
+
+        private Vector2 mousePosition;
+
+        [SerializeField]
+        private PolygonCollider2D upCollider;
+        [SerializeField]
+        private PolygonCollider2D downCollider;
+        [SerializeField]
+        private PolygonCollider2D leftCollider;
+        [SerializeField]
+        private PolygonCollider2D rightCollider;
+
+        [SerializeField]
+        private CircleCollider2D centerCollider;
     }
 }

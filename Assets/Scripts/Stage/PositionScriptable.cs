5fceea2dbbe03085a20599d095f4b5c4c6034819
SJ is free <dkstlzu@gmail.com>
2024-01-27 18:35:23 +0900

    Ingame scene added

---
+++ Assets/Scripts/Stage/PositionScriptable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace TouchToStart
+{
+    [CreateAssetMenu(fileName = "Positions", menuName = "Data/Positions", order = 0)]
+    public class PositionScriptable : ScriptableObject
+    {
+        public Vector2[] Values;
+    }
+}
